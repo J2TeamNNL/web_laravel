fa150268a200ed37beb7029fe101ad345e1a60fb
Nguyễn Nam Long <j2teamnnl@gmail.com>
2022-04-02 21:24:35 +0700

    template

--- resources/views/course/index.blade.php
+++ resources/views/course/index.blade.php
@@ -1,53 +1,64 @@
-@if ($errors->any())
-    <div class="alert alert-danger">
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
+@extends('layout.master')
+@section('content')
+    <div class='card'>
+        @if ($errors->any())
+            <div class="card-header">
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            </div>
+        @endif
+        <div class='card-body'>
+            <a class="btn btn-success" href="{{ route('courses.create') }}">
+                Thêm
+            </a>
+            <form class="float-right form-group form-inline">
+                <label class="mr-2">Search:</label>
+                <input type="search" name="q" value="{{ $search }}" class="form-control">
+            </form>
+            <table class="table table-striped">
+                <tr>
+                    <th>#</th>
+                    <th>Name</th>
+                    <th>Created At</th>
+                    <th>Edit</th>
+                    <th>Delete</th>
+                </tr>
+                @foreach ($data as $each)
+                    <tr>
+                        <td>
+                            {{ $each->id }}
+                        </td>
+                        <td>
+                            {{ $each->name }}
+                        </td>
+                        <td>
+                            {{ $each->year_created_at }}
+                        </td>
+                        <td>
+                            <a class="btn btn-primary" href="{{ route('courses.edit', $each) }}">
+                                Edit
+                            </a>
+                        </td>
+                        <td>
+                            <form action="{{ route('courses.destroy', $each) }}" method="post">
+                                @csrf
+                                @method('DELETE')
+                                <button class="btn btn-danger">Delete</button>
+                            </form>
+                        </td>
+                    </tr>
+                @endforeach
+            </table>
+            <nav>
+                <ul class="pagination pagination-rounded mb-0">
+                    {{ $data->links() }}
+                </ul>
+            </nav>
+        </div>
     </div>
-@endif
-<a href="{{ route('courses.create') }}">
-    Thêm
-</a>
-<table border="1" width="100%">
-    <caption>
-        <form>
-            Search:
-            <input type="search" name="q" value="{{ $search }}">
-        </form>
-    </caption>
-    <tr>
-        <th>#</th>
-        <th>Name</th>
-        <th>Created At</th>
-        <th>Edit</th>
-        <th>Delete</th>
-    </tr>
-    @foreach ($data as $each)
-        <tr>
-            <td>
-                {{ $each->id }}
-            </td>
-            <td>
-                {{ $each->name }}
-            </td>
-            <td>
-                {{ $each->year_created_at }}
-            </td>
-            <td>
-                <a href="{{ route('courses.edit', $each) }}">
-                    Edit
-                </a>
-            </td>
-            <td>
-                <form action="{{ route('courses.destroy', $each) }}" method="post">
-                    @csrf
-                    @method('DELETE')
-                    <button>Delete</button>
-                </form>
-            </td>
-        </tr>
-    @endforeach
-</table>
-{{ $data->links() }}
+@endsection
---
+++ resources/views/layout/master.blade.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8"/>
+    <title>Hyper - Responsive Bootstrap 4 Admin Dashboard</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta content="A fully featured admin theme which can be used to build CRM, CMS, etc." name="description"/>
+    <meta content="Coderthemes" name="author"/>
+    <!-- App favicon -->
+    <link rel="shortcut icon" href="assets/images/favicon.ico">
+
+    <!-- App css -->
+    <link href="{{ asset('css/icons.min.css') }}" rel="stylesheet" type="text/css"/>
+    <link href="{{ asset('css/app-creative.min.css') }}" rel="stylesheet" type="text/css"/>
+</head>
+
+<body data-layout-config='{"leftSideBarTheme":"dark","layoutBoxed":false, "leftSidebarCondensed":false, "leftSidebarScrollable":false,"darkMode":false, "showRightSidebarOnStart": true}'>
+@include('layout.sidebar')
+<div class="wrapper">
+    @include('layout.header')
+    <div class="content-page">
+        <div class="content">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="page-title-box">
+                            <h4 class="page-title">Courses</h4>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-12">
+                        @yield('content')
+                    </div>
+                </div>
+            </div>
+        </div>
+        @include('layout.footer')
+    </div>
+</div>
+
+
+<script src="{{ asset('js/vendor.min.js') }}"></script>
+<script src="{{ asset('js/app.min.js') }}"></script>
+</body>
+</html>
